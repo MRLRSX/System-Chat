0467a88f45fa7aa2bd86e089d2fb2bca24cc53f2
MRLRSX <lucas.rocha-dev@outlook.com>
2022-09-19 18:39:38 -0300

    Initial Commit

---
+++ src/view/Cliente.java
@@ -0,0 +1,19 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class Cliente extends JFrame {
+   
+	
+	public Cliente(){
+		setTitle("CHAT");
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setSize(500, 90);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
